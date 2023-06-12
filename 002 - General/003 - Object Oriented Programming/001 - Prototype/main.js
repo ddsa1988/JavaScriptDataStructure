aac5a4d3f0f799f09e508933e377ad0865e6a703
Diego Alexandre <ddsa.1988@outlook.com>
2023-06-11 21:08:13 -0300

    commit

--- 002 - General/003 - Object Oriented Programming/001 - Prototype/main.js
+++ 002 - General/003 - Object Oriented Programming/001 - Prototype/main.js
@@ -1,1 +1,34 @@
 "use strict";
+
+/* Prototype based class */
+
+function Person(firstName, lastName, birthyear) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthyear = birthyear;
+}
+
+Person.prototype.getAge = function (year) {
+    return year - this.birthyear;
+};
+
+const diego = new Person("Diego", "Alexandre", 1988);
+console.log(diego);
+console.log(diego.getAge(2023));
+console.log(diego.__proto__);
+
+console.log();
+
+/* Prototype Inherited based class */
+
+function Student(firstName, lastName, birthyear, id) {
+    Person.call(this, firstName, lastName, birthyear);
+    this.id = id;
+}
+
+Student.prototype = Object.create(Person.prototype);
+
+const amanda = new Student("Amanda", "Perna", 1993, 100);
+console.log(amanda);
+console.log(amanda.getAge(2023));
+console.log(amanda.__proto__);
--- 002 - General/003 - Object Oriented Programming/002 - Class/main.js
+++ 002 - General/003 - Object Oriented Programming/002 - Class/main.js
@@ -1,1 +1,73 @@
 "use strict";
+
+/* Parent class */
+
+class Person {
+    #firstName; // Private attribute
+    #lastName;
+    #birthyear;
+
+    constructor(firstName, lastName, birthyear) {
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setBirthyear(birthyear);
+    }
+
+    setFirstName(firstName) {
+        this.#firstName = firstName;
+    }
+
+    setLastName(lastName) {
+        this.#lastName = lastName;
+    }
+
+    getFullName() {
+        return this.#firstName + " " + this.#lastName;
+    }
+
+    setBirthyear(birthyear) {
+        this.#birthyear = birthyear;
+    }
+
+    getBirthyear() {
+        return this.#birthyear;
+    }
+
+    getAge(year) {
+        return year - this.#birthyear;
+    }
+}
+
+const diego = new Person("Diego", "Alexandre", 1988);
+
+console.log(diego);
+console.log(diego.getFullName());
+console.log(diego.getAge(2023));
+console.log(diego.__proto__);
+
+console.log();
+
+/* Child class */
+
+class Student extends Person {
+    #id;
+
+    constructor(firstName, lastName, birthyear, id) {
+        super(firstName, lastName, birthyear);
+        this.setId(id);
+    }
+
+    setId(id) {
+        this.#id = id;
+    }
+
+    getId() {
+        return this.#id;
+    }
+}
+
+const amanda = new Student("Amanda", "Perna", 1993, 100);
+console.log(amanda);
+console.log(amanda.getFullName());
+console.log(amanda.getAge(2023));
+console.log(amanda.__proto__);
