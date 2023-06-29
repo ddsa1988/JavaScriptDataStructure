306182192dececc24618a20a40dd6cafa4d580f7
Diego Alexandre <ddsa.1988@outlook.com>
2023-06-29 14:49:30 -0300

    commit

--- 004 - Stacks/stack-map.js
+++ 004 - Stacks/stack-map.js
@@ -2,13 +2,63 @@
 
 class Stack {
     #items = new Map();
-    #count = 0;
 
     constructor() {}
 
     push(element) {
-        this.#items.set(this.#count++, element);
+        this.#items.set(this.size(), element);
+    }
+
+    pop() {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+
+        const key = this.size() - 1;
+        const element = this.#items.get(key);
+        this.#items.delete(key);
+
+        return element;
+    }
+
+    peek() {
+        return this.isEmpty() ? undefined : this.#items.get(this.size() - 1);
+    }
+
+    size() {
+        return this.#items.size;
+    }
+
+    isEmpty() {
+        return this.#items.size === 0;
+    }
+
+    clear() {
+        this.#items.clear();
+    }
+
+    toString() {
+        if (this.isEmpty()) {
+            return "";
+        }
+
+        let string = "";
+
+        return string;
     }
 }
 
 const stack = new Stack();
+
+console.log(stack.isEmpty());
+stack.push(5);
+stack.push(8);
+console.log(stack.peek());
+stack.push(11);
+console.log(stack.size());
+console.log(stack.isEmpty());
+stack.push(15);
+console.log(stack.pop());
+console.log(stack.pop());
+console.log(stack.size());
+console.log(stack.toString());
